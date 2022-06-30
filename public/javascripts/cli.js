cbe24d431d553488f4e3075f7e03da0ccf32a67e
Guilherme Marra <geolerme@gmail.com>
2022-06-30 13:43:02 -0400

    fixed wss and tls features

--- public/javascripts/cli.js
+++ public/javascripts/cli.js
@@ -1,18 +1,29 @@
 //JS MQTT
 // Create a client instance
-client = new Paho.MQTT.Client("broker.mqttdashboard.com", Number(8000), "guima10");
+//client = new Paho.MQTT.Client("broker.mqttdashboard.com", Number(8000), "guima10");
+client = new Paho.MQTT.Client("broker.emqx.io", Number(8084), "guima10");
+// connection options - for EMQX - these data are enough, even using wss / ssl
+var options = {
+	useSSL: true,
+  userName : "guima10",
+  password : "teste123",
+  onSuccess: onConnect,
+    };
+//TLS Config
+//TLS_CERT_PATH = "/javascripts/broker.emqx.io-ca.crt"
+
 
 // set callback handlers
 client.onConnectionLost = onConnectionLost;
 client.onMessageArrived = onMessageArrived;
 
 // connect the client
-client.connect({onSuccess:onConnect});
+client.connect(options);
 
 
 // called when the client connects
 function onConnect() {
-  // Once a connection has been made, make a subscription and send a message.
+  // Once a connection has been made, subscribe to channels
   console.log("onConnect");
 
   // Recursive function for subscribing to 6 channels
@@ -22,11 +33,26 @@ function onConnect() {
     subscribeRecursion(i+1)
   }
   subscribeRecursion(1)
-
-  //message = new Paho.MQTT.Message("0.55");
-  //message.destinationName = "dash/canal1";
-  //client.send(message);
 }
+
+// simulate values for input elements
+function simulateInputs() {
+  //Random number in range (min,max)/100
+  var rdm = function(min, max) {
+
+    return (Math.floor(Math.random()*(max - min + 1)) + min)/100;
+  }
+  chanList = ['dash/canal1','dash/canal2','dash/canal3']
+  var intervalHandler = setInterval(function () {
+    for (let t of chanList){
+      message = new Paho.MQTT.Message(String(rdm(0,100)));
+      message.destinationName = t;
+      client.send(message);
+    }
+  }, 5000);
+
+}
+
 
 // called when the client loses its connection
 function onConnectionLost(responseObject) {
